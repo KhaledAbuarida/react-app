590a316965e6e58c386ed4e4dea8b553c992e54d
Abu3rida <khaledabuarida0@gmail.com>
2023-05-08 08:38:34 +0300

    conditional rendering

--- src/component/ListGroup.tsx
+++ src/component/ListGroup.tsx
@@ -1,24 +1,18 @@
-
-
-function ListGroup()
-{
-    const list = [
-        'New York',
-        'San Francisco',
-        'Tokyo',
-        'London',
-        'Paris'
-    ]
-
-    
-    return (
-        <>
-        <h1>Hello React</h1>
-        <ul className="list-group">
-            {list.map(item => <li key={item} className="list-group-item">{item}</li>)}
-        </ul>
+function ListGroup() {
+  let list = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  return (
+    <>
+      <h1>Hello React</h1>
+      {list.length === 0 && <p>item not fount :(</p>}
+      <ul className="list-group">
+        {list.map((item) => (
+          <li key={item} className="list-group-item">
+            {item}
+          </li>
+        ))}
+      </ul>
     </>
-    );
+  );
 }
 
 export default ListGroup;
